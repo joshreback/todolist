f739e8485cac47b64b739e052b5a6daaec614f0e
Josh Reback <joshr@edmodo.com>
2015-02-04 23:03:18 -0800

    basic working v0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(new_user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to categories_path, notice: "Signed Up!"
     else
       render "new"
