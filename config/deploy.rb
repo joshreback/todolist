967a9a0344eb93efc3ae172be1fd361bddd6873f
Josh Reback <joshr@edmodo.com>
2015-02-22 22:02:02 -0800

    precompiled assets in production

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :deploy_to, '/home/deploy/todolist'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml .env}
+set :linked_files, %w{.env}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
